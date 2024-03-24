9611ee4677b0e315cfcede88aa401dc940a446f0
CHEGEBB <chegephil24@gmail.com>
2024-03-24 19:33:59 +0300

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This manifest creates a file in tmp
+file { '/tmp/school':
+    ensure  => file,
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
+}
