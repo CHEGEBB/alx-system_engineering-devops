dfb5b51a0b7e3cf75736d01d29c95145bc56dd8c
CHEGEBB <chegephil24@gmail.com>
2024-03-26 09:16:41 +0300

    update

--- 0x0B-ssh/0-use_a_private_key.sh
+++ 0x0B-ssh/0-use_a_private_key.sh
@@ -2,4 +2,4 @@
 
 #This script script that uses ssh to connect to your server using the private key ~/.ssh/school with the user ubuntu.
 
-ssh -i ~/.ssh/school ubuntu@
+ssh -i ~/.ssh/school ubuntu@54.236.17.26
